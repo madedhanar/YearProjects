783099da7f48371452bbd78c66649463fa905195
madedhanar <50219598+madedhanar@users.noreply.github.com>
2020-04-26 14:25:24 +0700

    Add files via upload

---
+++ EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+ 
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var hardnessLabel: UILabel!
+    @IBOutlet weak var timerProgress: UIProgressView!
+    
+    let eggTime = ["Soft": 3, "Medium": 5, "Hard": 7]
+    
+    var timeRemaining = 1
+    var timer = Timer()
+    var totalTime = 0
+    var timePassed = 0
+    var player: AVAudioPlayer!
+    
+    @IBAction func eggTimer(_ sender: UIButton) {
+        timerProgress.progress = 0
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTime[hardness]!
+        timeRemaining = eggTime[hardness]! + 1
+        hardnessLabel.text = sender.currentTitle!
+        
+       timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
+    }
+    
+    @objc func countdown(){
+        if timePassed < totalTime {
+            print("\(timeRemaining) seconds.")
+            timePassed += 1
+            timeRemaining -= 1
+            timerLabel.text = String("\(timeRemaining)")
+            timerProgress.progress = Float(timePassed) / Float(totalTime)
+
+        } else {
+            timer.invalidate()
+            timePassed = 0
+            totalTime = 0
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+            timerLabel.text = "Done!"
+        }
+        
+        
+    }
+
+}
